9eba9da3637c2dc1ae190c4f6ab29da728e94432
Elena Uvarova <uvelmi@mail.ru>
2024-03-06 16:15:46 +0700

    changed eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,24 @@
 module.exports = {
-    env: {
-        browser: true,
-        es2021: true,
-    },
-    extends: ['eslint:recommended', 'prettier'],
-    overrides: [
-        {
-            env: {
-                node: true,
-            },
-            files: ['.eslintrc.{js,cjs}'],
-            parserOptions: {
-                sourceType: 'script',
-            },
-        },
-    ],
-    plugins: ['prettier'],
-    parserOptions: {
-        ecmaVersion: 'latest',
-        sourceType: 'module',
-    },
-    rules: {
-        'prettier/prettier': ['error', { endOfLine: 'auto' }],
-        camelcase: ['error'],
-        eqeqeq: ['error', 'always'],
-    },
+	env: {
+			browser: true,
+			es2021: true,
+	},
+	extends: ['eslint:recommended', 'prettier'],
+	overrides: [
+			{
+					env: {
+							node: true,
+					},
+					files: ['.eslintrc.{js,cjs}'],
+					parserOptions: {
+							sourceType: 'script',
+					},
+			},
+	],
+	plugins: ['prettier'],
+	parserOptions: {
+			ecmaVersion: 'latest',
+			sourceType: 'module',
+	},
+	rules: {},
 }
